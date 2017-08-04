854d25892769ef0fda8709601cb13a3403689b9a
唐钰豆豆 <doudoudzj@sina.com>
2017-08-04 11:50:05 +0800

    about me

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.info(
+  '%c' +
+  'About me:\n',
+  'text-shadow: 2px 0px 1px #333;color:blue;font-weight:bold;font-size:19px;'
+)
+console.info(
+  '%c' +
+  'blog:http://doudoudzj.com\n' +
+  'github:https://github.com/doudoudzj\n' +
+  'self-hosted git:https://git.crogram.com\n' +
+  'this project page:http://u-id.cn',
+  'text-shadow: 2px 0px 1px #eee;color:blue;font-size:16px;'
+)
